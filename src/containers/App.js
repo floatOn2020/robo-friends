24e4d38766843f1f3d20ef0bc57855ad74cb5c35
Bianca Herbst <b.herbst@posteo.de>
2020-05-28 11:41:30 +0200

    add thunk, add requestRobots action and reducer

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,41 +7,36 @@ import ErrorBoundry from '../components/ErrorBoundry';
 
 import 'tachyons'
 
-import{setSearchField} from '../actions'
+import{setSearchField, requestRobots} from '../actions'
 
 const mapStateToProps = state => {
   return{
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = dispatch => {
  return {
-  onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => requestRobots(dispatch)
 }}
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      robots: []
-    }
+
+  componentDidMount(){
+    this.props.onRequestRobots();
+
   }
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(users => this.setState({robots: users}));
-
-}
-
   render() {
-    const {robots} = this.state
-    const {searchField, onSearchChange} = this.props
+    const {searchField, onSearchChange, robots, isPending} = this.props
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase())
     })
-    return !robots.length ?
+    return isPending ?
       <h1>LOADING</h1> :
       (
         <div className="tc ">
